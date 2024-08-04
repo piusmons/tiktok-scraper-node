837fa7428bc51e49948e7754eb34e47cfc933777
Richard Chua <piuslchua@gmail.com>
2024-08-04 14:47:50 +0800

    testing another one richard

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-
-require('dotenv').config();
-const mongoose = require('mongoose');
-const puppeteerService = require('./services/puppeteerService');
-
+require("dotenv").config();
+const mongoose = require("mongoose");
+const puppeteerService = require("./services/puppeteerService");
 
 const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI)
-    console.log("connected to mongodb")
-  } catch (error) {
-    console.log(error);
-  }
-}
-    
+	try {
+		await mongoose.connect(process.env.MONGODB_URI);
+		console.log("connected to mongodb");
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 //Testing Comment
+// another test
 
-
-connect()
-puppeteerService.automateBrowser("#papaplatte",5,"papaplatte"); 
+connect();
+puppeteerService.automateBrowser("#papaplatte", 5, "papaplatte");
